32b99070c4181d886ee67348607ef700b0172581
ELKINS112 <58363457+ELKINS112@users.noreply.github.com>
2025-05-17 09:45:26 +0100

    Add files via upload

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,9 @@
+import axios from 'axios';
 
+const API_BASE = "https://hospital-backend-0laa.onrender.com";
+
+export const loginUser = async (credentials) => {
+  return axios.post(`${API_BASE}/login`, credentials, {
+    withCredentials: true
+  });
+};
