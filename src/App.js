0e3ec6bd735bc09e3a095e1d2f6554571aa9cec7
ELKINS112 <58363457+ELKINS112@users.noreply.github.com>
2025-05-17 09:49:38 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import AdminDashboard from './pages/AdminDashboard';
+import DoctorDashboard from './pages/DoctorDashboard';
+import NurseDashboard from './pages/NurseDashboard';
+import LabDashboard from './pages/LabDashboard';
+import PharmacistDashboard from './pages/PharmacistDashboard';
+import AccountantDashboard from './pages/AccountantDashboard';
+import PatientDashboard from './pages/PatientDashboard';
 
 function App() {
   return (
@@ -9,6 +15,12 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
+        <Route path="/nurse/dashboard" element={<NurseDashboard />} />
+        <Route path="/lab/dashboard" element={<LabDashboard />} />
+        <Route path="/pharmacist/dashboard" element={<PharmacistDashboard />} />
+        <Route path="/accountant/dashboard" element={<AccountantDashboard />} />
+        <Route path="/patient/dashboard" element={<PatientDashboard />} />
       </Routes>
     </Router>
   );
