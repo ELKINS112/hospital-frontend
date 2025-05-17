4ceff2a1a7bb9bef0e1a016bc68683cf196042d6
ELKINS112 <58363457+ELKINS112@users.noreply.github.com>
2025-05-17 19:44:26 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
+
 import axios from 'axios';
 
 const API_BASE = "https://hospital-backend-0laa.onrender.com";
 
 export const loginUser = async (credentials) => {
-  return axios.post(`${API_BASE}/login`, credentials, {
-    withCredentials: true
-  });
+  return axios.post(`${API_BASE}/login`, credentials);
 };
